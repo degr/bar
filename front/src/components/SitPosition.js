331351d0ce3037648bd36cad38f90e55b6489619
kalevalski <kalevalski@gmail.com>
2019-04-12 12:47:06 +0000

    test_01

--- front/src/components/SitPosition.js
+++ front/src/components/SitPosition.js
@@ -4,7 +4,7 @@ import DefaultLocations from '../utils/DefaultLocations';
 export default class SitPosition extends React.Component {
     render() {
         const data = DefaultLocations[this.props.index];
-        return <div style={{left: data.x, top: data.y}}
+        return <div style={{top: data.z*100, left: Math.abs(data.x*100)}}
                     className={"sit-position sit-position-" + this.props.index}
                     onClick={this.onClick}/>
     }
